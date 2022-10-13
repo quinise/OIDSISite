c03c964d6c061f4161412226a29c4e68f0c8c915
Devin Ercolano <devin.ercolano@gmail.com>
2022-10-13 08:37:36 -0700

    second commit, working on home.css

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require("path");
+const port = 3000;
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set("public", path.join(__dirname, "public"));
+
+app.get('/', function(req, res) {
+  res.render('home.ejs')})
+
+app.get('/events', function(req, res) {
+  res.render('events.ejs')})
+
+app.get('/readings', function(req, res) {
+  res.render('readings.ejs')})
+
+app.get('/contact', function(req, res) {
+  res.render('contact.ejs')})
+
+app.get('/about', function(req, res) {
+  res.render('about.ejs')})
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}` );
+});
---
+++ public/js/readings.js
@@ -0,0 +1,24 @@
+// Readings Link Page Data
+
+var priest_details = [{
+    priest_name: "Khepra Ptah",
+    link: "www.google.com",
+    photo_url: "/images/stock_profile.jpeg"
+},
+{
+    priest_name: "Iya Kim Early",
+    link: "www.google.com",
+    photo_url: "/images/stock_profile.jpeg"
+},
+{ 
+    priest_name: "Iya Omitosin",
+    link: "www.google.com",
+    photo_url: "/images/stock_profile.jpeg"
+
+}];
+
+$(window).on('load', function() {
+    for (i = 0; i < priest_details.length; i++) {
+        $('<div class="card_container" />').html('<div class="priest_card"><h2>' + priest_details[i].priest_name + '</h2><p><a class="priest_link" href=' + 'http://www.google.com' + '>'+ "Schedle a reading with " + priest_details[i].priest_name + '</a></p>' +'<img class="profile_pic" src=' + priest_details[i].photo_url + '><a href="Schedule a reading with' + priest_details[i].priest_name + '>link text</a></div>').appendTo('#cards');
+    }
+});
